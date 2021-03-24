26a2f56161957afb22f35dfe11d819e820d17a80
Andy Likuski <andy@likuski.org>
2021-03-24 12:12:22 +0100

    Added ability to add caller resources to default resources

--- src/i18n.js
+++ src/i18n.js
@@ -33,78 +33,92 @@ const resources = {
   }
 };
 
-export const mergedResources = () => R.compose(
-  R.fromPairs,
-  resources => chainObjToValues(
-    (baseLanguage, langKey) => {
-      const {default: defaultObj, ...variations} = baseLanguage;
-      const defaultResources = R.view(
-        R.lensPath(['resources', langKey]),
-        defaultObj
-      );
-      // Return the default with key langKey with variations concatinated.
-      const defaultPair = [langKey, defaultResources];
-      return R.concat([defaultPair], mapObjToValues(
-        (variationObj, variationKey) => {
-          // Makes the variation key pair, e.g. en-us
-          const variationPairKey = R.join('-', [langKey, variationKey]);
+/**
+ * Merges resources above with the caller's resources
+ * @param {Object} [callerResources] Default {} In the form of resources above. Deep merged with
+ * resources with this object favored
+ * @returns {Object}
+ */
+export const mergedResources = (callerResources = {}) => {
+  const allResources = mergeDeep(resources, callerResources);
+  return R.compose(
+    R.fromPairs,
+    resources => chainObjToValues(
+      (baseLanguage, langKey) => {
+        const {default: defaultObj, ...variations} = baseLanguage;
+        const defaultResources = R.view(
+          R.lensPath(['resources', langKey]),
+          defaultObj
+        );
+        // Return the default with key langKey with variations concatinated.
+        const defaultPair = [langKey, defaultResources];
+        return R.concat([defaultPair], mapObjToValues(
+          (variationObj, variationKey) => {
+            // Makes the variation key pair, e.g. en-us
+            const variationPairKey = R.join('-', [langKey, variationKey]);
 
-          // Returns pairs of variations with key langKey-variationKey
-          // Variations merge the default object with their object
-          return [variationPairKey,
-            R.compose(
-              // Merge the default obj
-              variationResources => {
-                return mergeDeep(
-                  defaultResources,
-                  variationResources
-                );
-              },
-              variationObj => {
-                return reqPathThrowing(['resources', variationPairKey], variationObj);
-              }
-            )(variationObj)
-          ];
-        },
-        variations
-      ));
-    },
-    resources)
-)(resources);
+            // Returns pairs of variations with key langKey-variationKey
+            // Variations merge the default object with their object
+            return [variationPairKey,
+              R.compose(
+                // Merge the default obj
+                variationResources => {
+                  return mergeDeep(
+                    defaultResources,
+                    variationResources
+                  );
+                },
+                variationObj => {
+                  return reqPathThrowing(['resources', variationPairKey], variationObj);
+                }
+              )(variationObj)
+            ];
+          },
+          variations
+        ));
+      },
+      resources)
+  )(allResources);
+};
 
 /**
  * Returns a task that when complete indicates that i18n is initialized
+ * @param {Object} [resources] If specified this is deep merged into the default resources
+ * declared above. It must match the form the of the resources above.
  * @returns The task resolves to the original i18n after it's initialized
  */
-export const i18nTask = fromPromised(
-  () => {
-    return i18n
-      .use(Backend)
-      .use(LanguageDetector)
-      .use(initReactI18next) // bind react-i18next to the instance
-      .init({
-          resources: mergedResources(),
-          fallbackLng: "en",
-          debug: true,
-          lowerCaseLng: true,
-          interpolation: {
-            escapeValue: false // not needed for react!!
-          }
+export const i18nTask = resources => {
+  return fromPromised(
+    resources => {
+      return i18n
+        .use(Backend)
+        .use(LanguageDetector)
+        .use(initReactI18next) // bind react-i18next to the instance
+        .init({
+            // Merge default resources with the caller's
+            resources: mergedResources(resources),
+            fallbackLng: "en",
+            debug: true,
+            lowerCaseLng: true,
+            interpolation: {
+              escapeValue: false // not needed for react!!
+            }
 
-          // react i18next special options (optional)
-          // override if needed - omit if ok with defaults
-          /*
-          react: {
-            bindI18n: 'languageChanged',
-            bindI18nStore: '',
-            transEmptyNodeValue: '',
-            transSupportBasicHtmlNodes: true,
-            transKeepBasicHtmlNodesFor: ['br', 'strong', 'i'],
-            useSuspense: true,
+            // react i18next special options (optional)
+            // override if needed - omit if ok with defaults
+            /*
+            react: {
+              bindI18n: 'languageChanged',
+              bindI18nStore: '',
+              transEmptyNodeValue: '',
+              transSupportBasicHtmlNodes: true,
+              transKeepBasicHtmlNodesFor: ['br', 'strong', 'i'],
+              useSuspense: true,
+            }
+            */
           }
-          */
-        }
-      ).then(() => i18n);
-  }
-)();
+        ).then(() => i18n);
+    }
+  )(resources);
+};
 
--- src/i18n.spec.js
+++ src/i18n.spec.js
@@ -3,7 +3,6 @@ import i18next from 'i18next';
 import {i18nTask, mergedResources} from './i18n.js';
 import * as R from 'ramda';
 import {composeWithChain, defaultRunConfig, omitDeepBy} from '@rescapes/ramda';
-import {pickBy} from 'ramda/src/index';
 import T from 'folktale/concurrency/task/index.js';
 
 const {fromPromised} = T;
@@ -36,11 +35,66 @@ describe('translation', () => {
       omitDeepBy((key, value) => {
           // Filter all resources for our expected data
           return R.complement(R.includes)(key,
-            ['en', 'en-us', 'no', 'no-nb', 'no-nn',
-              'lng', 'resources', 'translation', 'hello world', 'colloquial hello world']
+            [
+              // Just test these languages
+              'en', 'en-us', 'no', 'no-nb', 'no-nn',
+              'lng', 'resources', 'translation',
+              // Just test these keys
+              'hello world', 'colloquial hello world']
           );
         },
         mergedResources()
+      )
+    ).toEqual(expected);
+  });
+
+  test('mergedResourcesWithCallers', () => {
+    const callerResources = {
+      "en": {
+        "default": {
+          "resources": {
+            "en": {
+              "translation": {
+                // Override
+                "hello world": "hello dog"
+              }
+            }
+          }
+        }
+      },
+      "no": {
+        "default": {
+          "resources": {
+            "no": {
+              "translation": {
+                // Addition
+                "goodbye": "ha det bra"
+              }
+            }
+          }
+        }
+      }
+    };
+    const expected = {
+      "en": {"translation": {"colloquial hello world": "hi world", "hello world": "hello dog"}},
+      "en-us": {"translation": {"colloquial hello world": "howdy world", "hello world": "hello dog"}},
+      "no": {"translation": {"hello world": "hei", "goodbye": "ha det bra"}},
+      "no-nb": {"translation": {"hello world": "hei verden", "goodbye": "ha det bra"}},
+      "no-nn": {"translation": {"hello world": "hei verd", "goodbye": "ha det bra"}}
+    };
+    expect(
+      omitDeepBy((key, value) => {
+          // Filter all resources for our expected data
+          return R.complement(R.includes)(key,
+            [
+              // Just test these languages
+              'en', 'en-us', 'no', 'no-nb', 'no-nn',
+              'lng', 'resources', 'translation',
+              // Just test these keys
+              'hello world', 'colloquial hello world', 'goodbye']
+          );
+        },
+        mergedResources(callerResources)
       )
     ).toEqual(expected);
   });
@@ -49,9 +103,9 @@ describe('translation', () => {
     const errors = [];
     composeWithChain([
       i18next => fromPromised(lang => {
-        return i18next.changeLanguage(lang)
+        return i18next.changeLanguage(lang);
       })('no-nn'),
-      () => i18nTask
+      () => i18nTask()
     ])().run().listen(defaultRunConfig({
       onResolved: t => {
         expect(t('hello world')).toEqual('hei verd');
@@ -63,9 +117,9 @@ describe('translation', () => {
     const errors = [];
     composeWithChain([
       i18next => fromPromised(lang => {
-        return i18next.changeLanguage(lang)
+        return i18next.changeLanguage(lang);
       })('en-us'),
-      () => i18nTask
+      () => i18nTask()
     ])().run().listen(defaultRunConfig({
       onResolved: t => {
         expect(t('colloquial hello world')).toEqual('howdy world');
